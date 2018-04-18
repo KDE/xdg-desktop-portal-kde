0ff113d3dcbdece72bc90f2cdd81247bbaae27f2
Jan Grulich <jgrulich@redhat.com>
2018-04-18 11:46:59 +0200

    Search properly for pipewire and other libs
    Summary:
    We cannot rely on pkgconfig only so use more advanced search for libs
    required by screencast portal
    
    Reviewers: #plasma, cgiboudeaux
    
    Reviewed By: cgiboudeaux
    
    Subscribers: #plasma
    
    Differential Revision: https://phabricator.kde.org/D12147

--- cmake/modules/FindEpoxy.cmake
+++ cmake/modules/FindEpoxy.cmake
@@ -1,11 +1,11 @@
 # - Try to find libepoxy
 # Once done this will define
 #
-#  epoxy_FOUND        - System has libepoxy
-#  epoxy_LIBRARY      - The libepoxy library
-#  epoxy_INCLUDE_DIR  - The libepoxy include dir
-#  epoxy_DEFINITIONS  - Compiler switches required for using libepoxy
-#  epoxy_HAS_GLX      - Whether GLX support is available
+#  Epoxy_FOUND        - System has libepoxy
+#  Epoxy_LIBRARIES      - The libepoxy library
+#  Epoxy_INCLUDE_DIRS  - The libepoxy include dir
+#  Epoxy_DEFINITIONS  - Compiler switches required for using libepoxy
+#  Epoxy_HAS_GLX      - Whether GLX support is available
 
 # Copyright (c) 2014 Fredrik Höglund <fredrik@kde.org>
 #
@@ -33,24 +33,22 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-if (NOT WIN32)
-  find_package(PkgConfig)
-  pkg_check_modules(PKG_epoxy QUIET epoxy)
+find_package(PkgConfig)
+pkg_check_modules(PKG_Epoxy QUIET epoxy)
 
-  set(epoxy_DEFINITIONS ${PKG_epoxy_CFLAGS})
+set(Epoxy_DEFINITIONS ${PKG_Epoxy_CFLAGS})
 
-  find_path(epoxy_INCLUDE_DIR NAMES epoxy/gl.h HINTS ${PKG_epoxy_INCLUDEDIR} ${PKG_epoxy_INCLUDE_DIRS})
-  find_library(epoxy_LIBRARY  NAMES epoxy      HINTS ${PKG_epoxy_LIBDIR} ${PKG_epoxy_LIBRARY_DIRS})
-  find_file(epoxy_GLX_HEADER NAMES epoxy/glx.h HINTS ${epoxy_INCLUDE_DIR})
+find_path(Epoxy_INCLUDE_DIRS NAMES epoxy/gl.h HINTS ${PKG_Epoxy_INCLUDEDIR} ${PKG_Epoxy_INCLUDE_DIRS})
+find_library(Epoxy_LIBRARIES  NAMES epoxy      HINTS ${PKG_Epoxy_LIBDIR} ${PKG_Epoxy_LIBRARIES_DIRS})
+find_file(Epoxy_GLX_HEADER NAMES epoxy/glx.h HINTS ${Epoxy_INCLUDE_DIRS})
 
-  if (epoxy_GLX_HEADER STREQUAL "epoxy_GLX_HEADER-NOTFOUND")
-    set(epoxy_HAS_GLX FALSE CACHE BOOL "whether glx is available")
-  else ()
-    set(epoxy_HAS_GLX TRUE CACHE BOOL "whether glx is available")
-  endif()
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(epoxy DEFAULT_MSG epoxy_LIBRARY epoxy_INCLUDE_DIR)
-
-  mark_as_advanced(epoxy_INCLUDE_DIR epoxy_LIBRARY epoxy_HAS_GLX)
+if (Epoxy_GLX_HEADER STREQUAL "Epoxy_GLX_HEADER-NOTFOUND")
+set(Epoxy_HAS_GLX FALSE CACHE BOOL "whether glx is available")
+else ()
+set(Epoxy_HAS_GLX TRUE CACHE BOOL "whether glx is available")
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Epoxy DEFAULT_MSG Epoxy_LIBRARIES Epoxy_INCLUDE_DIRS)
+
+mark_as_advanced(Epoxy_INCLUDE_DIRS Epoxy_LIBRARIES Epoxy_HAS_GLX)
--- cmake/modules/FindGBM.cmake
+++ cmake/modules/FindGBM.cmake
@@ -1,28 +1,28 @@
 #.rst:
-# Findgbm
+# FindGBM
 # -------
 #
 # Try to find gbm on a Unix system.
 #
 # This will define the following variables:
 #
-# ``gbm_FOUND``
+# ``GBM_FOUND``
 #     True if (the requested version of) gbm is available
-# ``gbm_VERSION``
+# ``GBM_VERSION``
 #     The version of gbm
-# ``gbm_LIBRARIES``
-#     This can be passed to target_link_libraries() instead of the ``gbm::gbm``
+# ``GBM_LIBRARIES``
+#     This can be passed to target_link_libraries() instead of the ``GBM::GBM``
 #     target
-# ``gbm_INCLUDE_DIRS``
+# ``GBM_INCLUDE_DIRSS``
 #     This should be passed to target_include_directories() if the target is not
 #     used for linking
-# ``gbm_DEFINITIONS``
+# ``GBM_DEFINITIONS``
 #     This should be passed to target_compile_options() if the target is not
 #     used for linking
 #
-# If ``gbm_FOUND`` is TRUE, it will also define the following imported target:
+# If ``GBM_FOUND`` is TRUE, it will also define the following imported target:
 #
-# ``gbm::gbm``
+# ``GBM::GBM``
 #     The gbm library
 #
 # In general we recommend using the imported target, as it is easier to use.
@@ -57,69 +57,51 @@
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #=============================================================================
 
-if(CMAKE_VERSION VERSION_LESS 2.8.12)
-    message(FATAL_ERROR "CMake 2.8.12 is required by Findgbm.cmake")
-endif()
-if(CMAKE_MINIMUM_REQUIRED_VERSION VERSION_LESS 2.8.12)
-    message(AUTHOR_WARNING "Your project should require at least CMake 2.8.12 to use Findgbm.cmake")
-endif()
+# Use pkg-config to get the directories and then use these values
+# in the FIND_PATH() and FIND_LIBRARY() calls
+find_package(PkgConfig)
+pkg_check_modules(PKG_GBM QUIET gbm)
 
-if(NOT WIN32)
-    # Use pkg-config to get the directories and then use these values
-    # in the FIND_PATH() and FIND_LIBRARY() calls
-    find_package(PkgConfig)
-    pkg_check_modules(PKG_gbm QUIET gbm)
+set(GBM_DEFINITIONS ${PKG_GBM_CFLAGS_OTHER})
+set(GBM_VERSION ${PKG_GBM_VERSION})
 
-    set(gbm_DEFINITIONS ${PKG_gbm_CFLAGS_OTHER})
-    set(gbm_VERSION ${PKG_gbm_VERSION})
+find_path(GBM_INCLUDE_DIRS
+    NAMES
+        gbm.h
+    HINTS
+        ${PKG_GBM_INCLUDE_DIRS}
+)
+find_library(GBM_LIBRARIES
+    NAMES
+        gbm
+    HINTS
+        ${PKG_GBM_LIBRARIES_DIRS}
+)
 
-    find_path(gbm_INCLUDE_DIR
-        NAMES
-            gbm.h
-        HINTS
-            ${PKG_gbm_INCLUDE_DIRS}
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GBM
+    FOUND_VAR
+        GBM_FOUND
+    REQUIRED_VARS
+        GBM_LIBRARIES
+        GBM_INCLUDE_DIRS
+    VERSION_VAR
+        GBM_VERSION
+)
+
+if(GBM_FOUND AND NOT TARGET GBM::GBM)
+    add_library(GBM::GBM UNKNOWN IMPORTED)
+    set_target_properties(GBM::GBM PROPERTIES
+        IMPORTED_LOCATION "${GBM_LIBRARIES}"
+        INTERFACE_COMPILE_OPTIONS "${GBM_DEFINITIONS}"
+        INTERFACE_INCLUDE_DIRECTORIES "${GBM_INCLUDE_DIRS}"
     )
-    find_library(gbm_LIBRARY
-        NAMES
-            gbm
-        HINTS
-            ${PKG_gbm_LIBRARY_DIRS}
-    )
-
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(gbm
-        FOUND_VAR
-            gbm_FOUND
-        REQUIRED_VARS
-            gbm_LIBRARY
-            gbm_INCLUDE_DIR
-        VERSION_VAR
-            gbm_VERSION
-    )
-
-    if(gbm_FOUND AND NOT TARGET gbm::gbm)
-        add_library(gbm::gbm UNKNOWN IMPORTED)
-        set_target_properties(gbm::gbm PROPERTIES
-            IMPORTED_LOCATION "${gbm_LIBRARY}"
-            INTERFACE_COMPILE_OPTIONS "${gbm_DEFINITIONS}"
-            INTERFACE_INCLUDE_DIRECTORIES "${gbm_INCLUDE_DIR}"
-        )
-    endif()
-
-    mark_as_advanced(gbm_LIBRARY gbm_INCLUDE_DIR)
-
-    # compatibility variables
-    set(gbm_LIBRARIES ${gbm_LIBRARY})
-    set(gbm_INCLUDE_DIRS ${gbm_INCLUDE_DIR})
-    set(gbm_VERSION_STRING ${gbm_VERSION})
-
-else()
-    message(STATUS "Findgbm.cmake cannot find gbm on Windows systems.")
-    set(gbm_FOUND FALSE)
 endif()
+
+mark_as_advanced(GBM_LIBRARIES GBM_INCLUDE_DIRS)
 
 include(FeatureSummary)
-set_package_properties(gbm PROPERTIES
+set_package_properties(GBM PROPERTIES
     URL "http://www.mesa3d.org"
     DESCRIPTION "Mesa gbm library."
 )
