9231501eeb7371eb0986d2911ee31110a8b9bc10
Christophe Giboudeaux <christophe@krop.fr>
2018-04-18 12:42:46 +0200

    Simplify.
    Epoxy_GLX_HEADER-NOTFOUND already evaluates to FALSE

--- cmake/modules/FindEpoxy.cmake
+++ cmake/modules/FindEpoxy.cmake
@@ -40,13 +40,7 @@ set(Epoxy_DEFINITIONS ${PKG_Epoxy_CFLAGS})
 
 find_path(Epoxy_INCLUDE_DIRS NAMES epoxy/gl.h HINTS ${PKG_Epoxy_INCLUDEDIR} ${PKG_Epoxy_INCLUDE_DIRS})
 find_library(Epoxy_LIBRARIES  NAMES epoxy      HINTS ${PKG_Epoxy_LIBDIR} ${PKG_Epoxy_LIBRARIES_DIRS})
-find_file(Epoxy_GLX_HEADER NAMES epoxy/glx.h HINTS ${Epoxy_INCLUDE_DIRS})
-
-if (Epoxy_GLX_HEADER STREQUAL "Epoxy_GLX_HEADER-NOTFOUND")
-set(Epoxy_HAS_GLX FALSE CACHE BOOL "whether glx is available")
-else ()
-set(Epoxy_HAS_GLX TRUE CACHE BOOL "whether glx is available")
-endif()
+find_file(Epoxy_GLX_HEADER NAMES epoxy/glx.h HINTS ${Epoxy_INCLUDE_DIRS} DOC "whether GLX is available")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Epoxy DEFAULT_MSG Epoxy_LIBRARIES Epoxy_INCLUDE_DIRS)
