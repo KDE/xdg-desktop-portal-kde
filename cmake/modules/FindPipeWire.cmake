05a9caf5e866446d3d1878f58b215ddcea4b6488
Jan Grulich <jgrulich@redhat.com>
2020-02-10 16:22:31 +0100

    Properly search for PipeWire libs
    Summary:
    Recent change in upcoming PipeWire releases made all headers to be
    installed versioned directories so multiple versions can be installed
    at once. We have to search a bit differently now. This change makes
    it to search first for PipeWire 0.3 and if it's not found, we try to
    search for the previous version.
    
    Reviewers: #plasma, cgiboudeaux
    
    Reviewed By: cgiboudeaux
    
    Subscribers: cgiboudeaux, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D27277

--- cmake/modules/FindPipeWire.cmake
+++ cmake/modules/FindPipeWire.cmake
@@ -32,7 +32,7 @@
 #=============================================================================
 # Copyright 2014 Alex Merry <alex.merry@kde.org>
 # Copyright 2014 Martin Gräßlin <mgraesslin@kde.org>
-# Copyright 2018 Jan Grulich <jgrulich@redhat.com>
+# Copyright 2018-2020 Jan Grulich <jgrulich@redhat.com>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -61,9 +61,11 @@
 # Use pkg-config to get the directories and then use these values
 # in the FIND_PATH() and FIND_LIBRARY() calls
 find_package(PkgConfig QUIET)
-pkg_check_modules(PKG_PipeWire QUIET libpipewire-0.2 libpipewire-0.3)
 
-set(PipeWire_DEFINITIONS "${PKG_PipeWire_CFLAGS_OTHER}")
+pkg_search_module(PKG_PipeWire QUIET libpipewire-0.3 libpipewire-0.2)
+pkg_search_module(PKG_Spa QUIET libspa-0.2 libspa-0.1)
+
+set(PipeWire_DEFINITIONS "${PKG_PipeWire_CFLAGS}" "${PKG_Spa_CFLAGS}")
 set(PipeWire_VERSION "${PKG_PipeWire_VERSION}")
 
 find_path(PipeWire_INCLUDE_DIRS
@@ -71,13 +73,23 @@ find_path(PipeWire_INCLUDE_DIRS
         pipewire/pipewire.h
     HINTS
         ${PKG_PipeWire_INCLUDE_DIRS}
+        ${PKG_PipeWire_INCLUDE_DIRS}/pipewire-0.3
+)
+
+find_path(Spa_INCLUDE_DIRS
+    NAMES
+        spa/param/props.h
+    HINTS
+        ${PKG_Spa_INCLUDE_DIRS}
+        ${PKG_Spa_INCLUDE_DIRS}/spa-0.2
 )
 
 find_library(PipeWire_LIBRARIES
     NAMES
-        pipewire-0.2 pipewire-0.3
+        pipewire-0.3
+        pipewire-0.2
     HINTS
-        ${PKG_PipeWire_LIBRARIES_DIRS}
+        ${PKG_PipeWire_LIBRARY_DIRS}
 )
 
 include(FindPackageHandleStandardArgs)
@@ -87,6 +99,7 @@ find_package_handle_standard_args(PipeWire
     REQUIRED_VARS
         PipeWire_LIBRARIES
         PipeWire_INCLUDE_DIRS
+        Spa_INCLUDE_DIRS
     VERSION_VAR
         PipeWire_VERSION
 )
@@ -96,7 +109,7 @@ if(PipeWire_FOUND AND NOT TARGET PipeWire::PipeWire)
     set_target_properties(PipeWire::PipeWire PROPERTIES
         IMPORTED_LOCATION "${PipeWire_LIBRARIES}"
         INTERFACE_COMPILE_OPTIONS "${PipeWire_DEFINITIONS}"
-        INTERFACE_INCLUDE_DIRECTORIES "${PipeWire_INCLUDE_DIRS}"
+        INTERFACE_INCLUDE_DIRECTORIES "${PipeWire_INCLUDE_DIRS}" "${Spa_INCLUDE_DIRS}"
     )
 endif()
 
