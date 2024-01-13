KDE/xdg-desktop-portal-kde

commit a88216a4aa628b956eda3b8a0c5859e998f4f5bf

qml cleanup: removed duplicate toggled signal
Kirigami.Card is an ItemDelegate, which already defines the toggled
signal. Previously this line generates a duplicate signal warning:
"invalid override of property change signal or superclass signal".

src/PipeWireDelegate.qml
@@ -14,9 +14,6 @@ Kirigami.Card {
 
     property alias nodeId: pipeWireSourceItem.nodeId
 
-    // TODO: Card is an ItemDelegate, why it needs a toggled signal??
-    signal toggled()
-
     contentItem: PipeWire.PipeWireSourceItem {
         id: pipeWireSourceItem
         Layout.preferredHeight: Kirigami.Units.gridUnit * 7

commit 56b436375b2b5f4bff9f859792f9c717f82bf364

Don't call toggled twice
Since toggled was also triggered by onClicked, onToggled will be
called twice, effectively canceling out the toggle.
Fixes a regression caused by a88216a4aa628b956eda3b8a0c5859e998f4f5bf

src/PipeWireDelegate.qml
@@ -27,8 +27,4 @@ Kirigami.Card {
             source: card.banner.titleIcon
         }
     }
-
-    onClicked: {
-        toggled()
-    }
 }