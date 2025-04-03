ec70741cd89c9ba84956b2654e36775dfcf75477
David Edmundson <kde@davidedmundson.co.uk>
2025-04-03 13:22:59 +0100

    InhibitPortal: Implement SessionMonitor
    This is used for applications to inhibit logout.
    
    The design is that plasma-shutdown will call into here to query if log
    out is possible; this will in turn ask all applications if it is ok to
    quit.
    
    This makes it slightly different from other portals in that we have
    non-sandboxed parts calling into here.
    
    This patch also fixes a potential race with power inhibition if a caller
    closes the request super early.

--- src/inhibit.cpp
+++ src/inhibit.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: LGPL-2.0-or-later
  *
  * SPDX-FileCopyrightText: 2017 Jan Grulich <jgrulich@redhat.com>
+ * SPDX-FileCopyrightText: 2025 David Edmundson <davidedmundson@kde.org>
  */
 
 #include "inhibit.h"
@@ -14,10 +15,90 @@
 #include <QDBusPendingCall>
 #include <QDBusPendingCallWatcher>
 #include <QDBusPendingReply>
+#include <QTimer>
+
+class SessionStateMonitorSession : public Session
+{
+    Q_OBJECT
+public:
+    SessionStateMonitorSession(QObject *parent = nullptr, const QString &appId = QString(), const QString &path = QString());
+    ~SessionStateMonitorSession() = default;
+    SessionType type() const override
+    {
+        return SessionStateMonitor;
+    }
+    void setWaitingForResponse(bool waiting)
+    {
+        m_waitingForResponse = waiting;
+    }
+    bool waitingForResponse() const
+    {
+        return m_waitingForResponse;
+    }
+
+private:
+    bool m_waitingForResponse = false;
+};
+
+class SessionStateAdaptor : public QObject, public QDBusContext
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.SessionState")
+
+public:
+    explicit SessionStateAdaptor(InhibitPortal *parent = nullptr);
+
+public Q_SLOTS:
+    bool queryCanEndSession()
+    {
+        m_inhibitPortal->queryCanEndSession(message());
+        return true; // reply value will be ignored, handled async above
+    }
+    void endSession()
+    {
+        m_inhibitPortal->endSession();
+    }
+
+private:
+    InhibitPortal *m_inhibitPortal;
+};
+
+SessionStateAdaptor::SessionStateAdaptor(InhibitPortal *parent)
+    : QObject(parent)
+    , m_inhibitPortal(parent)
+{
+    QDBusConnection::sessionBus().registerObject(QStringLiteral("/org/kde/SessionState"), this, QDBusConnection::ExportAllSlots);
+}
 
 InhibitPortal::InhibitPortal(QObject *parent)
     : QDBusAbstractAdaptor(parent)
 {
+    new SessionStateAdaptor(this);
+
+    QDBusConnection::sessionBus().connect(QStringLiteral("org.freedesktop.ScreenSaver"),
+                                          QStringLiteral("/ScreenSaver"),
+                                          QStringLiteral("org.freedesktop.ScreenSaver"),
+                                          QStringLiteral("ActiveChanged"),
+                                          this, SLOT(lockScreenStateChanged(bool)));
+
+
+    auto isLocked = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.ScreenSaver"),
+                                                   QStringLiteral("/ScreenSaver"),
+                                                   QStringLiteral("org.freedesktop.ScreenSaver"),
+                                                   QStringLiteral("GetActive"));
+    QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(QDBusConnection::sessionBus().asyncCall(isLocked));
+    connect(watcher, &QDBusPendingCallWatcher::finished, this, [this](QDBusPendingCallWatcher *watcher) {
+        QDBusPendingReply<bool> reply = *watcher;
+        watcher->deleteLater();
+        if (reply.isError()) {
+            qCDebug(XdgDesktopPortalKdeInhibit) << "Error getting initial screen lock state: " << reply.error().message();
+            return;
+        }
+        if (reply.value()) {
+            lockScreenStateChanged(true);
+        } // else we're already locked, we can do nothing
+    });
+
 }
 
 void InhibitPortal::Inhibit(const QDBusObjectPath &handle, const QString &app_id, const QString &window, uint flags, const QVariantMap &options)
@@ -28,6 +109,15 @@ void InhibitPortal::Inhibit(const QDBusObjectPath &handle, const QString &app_id
     qCDebug(XdgDesktopPortalKdeInhibit) << "    window: " << window;
     qCDebug(XdgDesktopPortalKdeInhibit) << "    flags: " << flags;
     qCDebug(XdgDesktopPortalKdeInhibit) << "    options: " << options;
+
+    auto request = new Request(handle, this, QStringLiteral("org.freedesktop.impl.portal.Inhibit"));
+
+    if (flags & 1) { // Block logout
+        m_appsBlockingLogout[handle] = app_id;
+        connect(request, &Request::closeRequested, this, [this, handle]() {
+            m_appsBlockingLogout.remove(handle);
+        });
+    }
 
     QDBusMessage message = QDBusMessage::createMethodCall(QStringLiteral("org.kde.Solid.PowerManagement"),
                                                           QStringLiteral("/org/kde/Solid/PowerManagement/PolicyAgent"),
@@ -41,21 +131,32 @@ void InhibitPortal::Inhibit(const QDBusObjectPath &handle, const QString &app_id
         policies |= 4; // ChangeScreenSettings a.k.a. logind "idle"
     }
     if (policies == 0) {
-        qCDebug(XdgDesktopPortalKdeInhibit) << "Inhibition error: flags not supported by KDE policy agent:" << flags;
         return;
     }
     message << policies << app_id << options.value(QStringLiteral("reason")).toString();
 
     QDBusPendingCall pendingCall = QDBusConnection::sessionBus().asyncCall(message);
     QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(pendingCall);
-    connect(watcher, &QDBusPendingCallWatcher::finished, [handle, this](QDBusPendingCallWatcher *watcher) {
+
+    connect(watcher, &QDBusPendingCallWatcher::finished, this, [handle, this, request = QPointer<Request>(request)](QDBusPendingCallWatcher *watcher) {
         QDBusPendingReply<uint> reply = *watcher;
+        watcher->deleteLater();
         if (reply.isError()) {
             qCDebug(XdgDesktopPortalKdeInhibit) << "Inhibition error: " << reply.error().message();
         } else {
             QDBusConnection sessionBus = QDBusConnection::sessionBus();
             auto inhibitId = reply.value();
-            auto request = new Request(handle, this, QStringLiteral("org.freedesktop.impl.portal.Inhibit"), QVariant(inhibitId));
+
+            if (!request) {
+                QDBusMessage uninhibitMessage = QDBusMessage::createMethodCall(QStringLiteral("org.kde.Solid.PowerManagement"),
+                                                                               QStringLiteral("/org/kde/Solid/PowerManagement/PolicyAgent"),
+                                                                               QStringLiteral("org.kde.Solid.PowerManagement.PolicyAgent"),
+                                                                               QStringLiteral("ReleaseInhibition"));
+
+                uninhibitMessage << inhibitId;
+                QDBusConnection::sessionBus().asyncCall(uninhibitMessage);
+                return;
+            }
 
             // Once the request is closed by the application, release our inhibitor
             connect(request, &Request::closeRequested, this, [this, inhibitId] {
@@ -68,7 +169,8 @@ void InhibitPortal::Inhibit(const QDBusObjectPath &handle, const QString &app_id
 
                 QDBusPendingCall pendingCall = QDBusConnection::sessionBus().asyncCall(uninhibitMessage);
                 QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(pendingCall);
-                connect(watcher, &QDBusPendingCallWatcher::finished, this, [this, uninhibitMessage](QDBusPendingCallWatcher *watcher) {
+                connect(watcher, &QDBusPendingCallWatcher::finished, this, [uninhibitMessage](QDBusPendingCallWatcher *watcher) {
+                    watcher->deleteLater();
                     QDBusPendingReply<> reply = *watcher;
                     QDBusMessage messageReply;
                     if (reply.isError()) {
@@ -82,3 +184,112 @@ void InhibitPortal::Inhibit(const QDBusObjectPath &handle, const QString &app_id
         }
     });
 }
+
+uint InhibitPortal::CreateMonitor(const QDBusObjectPath &handle, const QDBusObjectPath &session_handle, const QString &app_id, const QString &window)
+{
+    if (m_monitors.contains(session_handle)) {
+        qCDebug(XdgDesktopPortalKdeInhibit) << "CreateMonitor called for existing session:" << session_handle;
+        return PortalResponse::OtherError;
+    }
+    Q_UNUSED(window);
+    Q_UNUSED(handle);
+
+    auto session = new SessionStateMonitorSession(this, app_id, session_handle.path());
+
+    auto sessionState = Running;
+
+    // XDP doesn't forward the initial state until we've completed creating the monitor
+    QTimer::singleShot(0, session, [session, this, sessionState] {
+        updateState(session, m_lockState, sessionState);
+    });
+
+    connect(session, &Session::closed, this, [this, session_handle]() {
+        m_monitors.remove(session_handle);
+    });
+    m_monitors.insert(session_handle, session);
+
+    return PortalResponse::Success;
+}
+
+void InhibitPortal::QueryEndResponse(const QDBusObjectPath &handle)
+{
+    auto session = m_monitors.value(handle);
+    if (!session) {
+        return;
+    }
+    session->setWaitingForResponse(false);
+    bool anySessionWaiting = std::any_of(m_monitors.constBegin(), m_monitors.constEnd(), [](SessionStateMonitorSession *session) {
+        return session->waitingForResponse();
+    });
+    if (!anySessionWaiting) {
+        queryCanShutDownComplete();
+    }
+}
+
+void InhibitPortal::lockScreenStateChanged(bool state)
+{
+    m_lockState = state ? LockScreenActive : LockScreenInactive;
+    for (auto session : std::as_const(m_monitors)) {
+        updateState(session, m_lockState, Running);
+        session->setWaitingForResponse(true);
+    }
+}
+
+void InhibitPortal::queryCanEndSession(const QDBusMessage &message)
+{
+    if (m_pendingCanShutDownReply.type() != QDBusMessage::InvalidMessage) {
+        qCWarning(XdgDesktopPortalKdeInhibit) << "querying shutdown before previous call completed, this may skip clients";
+    }
+
+    m_pendingCanShutDownReply = message;
+    message.setDelayedReply(true);
+
+    auto sessionState = QueryEnd;
+    for (auto session : std::as_const(m_monitors)) {
+        updateState(session, m_lockState, sessionState);
+        session->setWaitingForResponse(true);
+    }
+
+    if (m_monitors.isEmpty()) {
+        queryCanShutDownComplete();
+    } else {
+        // apps are meant to reply in 1 second, if an app gets stuck and fails to respond, reply to our query anyway.
+        QTimer::singleShot(std::chrono::milliseconds(1500), this, &InhibitPortal::queryCanShutDownComplete);
+    }
+}
+
+void InhibitPortal::endSession()
+{
+    auto sessionState = Ending;
+    for (auto session : std::as_const(m_monitors)) {
+        updateState(session, m_lockState, sessionState);
+    }
+}
+
+void InhibitPortal::queryCanShutDownComplete()
+{
+    if (m_pendingCanShutDownReply.type() != QDBusMessage::MethodCallMessage) {
+        return;
+    }
+
+    QDBusMessage message = m_pendingCanShutDownReply.createReply();
+    message << m_appsBlockingLogout.isEmpty();
+    QDBusConnection::sessionBus().send(message);
+    m_pendingCanShutDownReply = QDBusMessage();
+}
+
+void InhibitPortal::updateState(SessionStateMonitorSession *session, LockScreenState lockState, SessionState sessionState)
+{
+    QVariantMap data;
+    data[QStringLiteral("screensaver-active")] = (lockState == LockScreenActive);
+    data[QStringLiteral("session-state")] = static_cast<uint>(sessionState);
+
+    Q_EMIT StateChanged(QDBusObjectPath(session->handle()), data);
+}
+
+SessionStateMonitorSession::SessionStateMonitorSession(QObject *parent, const QString &appId, const QString &path)
+    : Session(parent, appId, path)
+{
+}
+
+#include "inhibit.moc"
